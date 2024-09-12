53b41741e04097c0378ebf9a370bfe9185e4881c
data-en <scientistdata953@gmail.com>
2024-09-12 15:29:43 +0530

    model trainer added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MLOps-US-Machine-Learning-Project-
+
+### Workflow 
+1. constants 
+2. entity 
+3. components 
+4. pipeline
+5. Main file 
--- us_visa/entity/artifact_entity.py
+++ us_visa/entity/artifact_entity.py
@@ -4,7 +4,8 @@ from dataclasses import dataclass
 @dataclass
 class DataIngestionArtifact:
     trained_file_path:str 
-    test_file_path:str  
+    test_file_path:str 
+
 
 
 @dataclass
@@ -12,3 +13,25 @@ class DataValidationArtifact:
     validation_status:bool
     message: str
     drift_report_file_path: str
+
+
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path:str 
+    transformed_train_file_path:str
+    transformed_test_file_path:str
+
+
+@dataclass
+class ClassificationMetricArtifact:
+    f1_score:float
+    precision_score:float
+    recall_score:float
+
+
+
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path:str 
+    metric_artifact:ClassificationMetricArtifact
